69431e028a81413a498b1bd7a8c3907351abb634
G.YUVA KISHORE REDDY <156158020+gyuvakishorereddy@users.noreply.github.com>
2026-01-08 20:30:25 +0530

    Fix config: SQL + MSAL redirect

--- config.py
+++ config.py
@@ -1,28 +1,37 @@
 import os
+from urllib.parse import quote_plus
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
+    # Blob Storage
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsstoragekishore'
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY')
 
+    # SQL Server
     SQL_SERVER = os.environ.get('SQL_SERVER')
     SQL_DATABASE = os.environ.get('SQL_DATABASE')
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME')
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
 
+    encoded_password = quote_plus(SQL_PASSWORD)
+
     SQLALCHEMY_DATABASE_URI = (
-        "mssql+pyodbc://cmsadmin:Admin%40123@cmsadmin.database.windows.net:1433/"
-    "cmsadmin?driver=ODBC+Driver+17+for+SQL+Server"
+        f"mssql+pyodbc://{SQL_USER_NAME}:{encoded_password}@{SQL_SERVER}:1433/"
+        f"{SQL_DATABASE}"
+        "?driver=ODBC+Driver+18+for+SQL+Server"
+        "&Encrypt=yes"
+        "&TrustServerCertificate=no"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # Azure AD / MSAL
     CLIENT_ID = os.environ.get('CLIENT_ID')
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-
     AUTHORITY = "https://login.microsoftonline.com/common"
     REDIRECT_PATH = "/getAToken"
     SCOPE = ["User.Read"]
+
     SESSION_TYPE = "filesystem"
